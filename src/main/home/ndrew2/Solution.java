343c030f9c42e97eb260a0ae34642263db370382
ndrew219 <Solembum@mail.ru>
2017-06-05 00:07:39 +0300

    up 04.06

--- src/main/home/ndrew2/Solution.java
+++ src/main/home/ndrew2/Solution.java
@@ -1,37 +1,38 @@
+package home.ndrew2;
+
 import java.util.ArrayList;
 
 /**
  * Created by Андрей on 28.05.2017.
+ * update 04.06.2017
  */
 public class Solution {
 
-    final private static int ARRAY_OVERSIZE_ERROR_CODE = -1;
-    final private static int ARRAY_NULL_ERROR_CODE = -3;
+    final public static int ARRAY_OVERSIZE_ERROR_CODE = -1;
+    final public static int ELEMENT_OF_ARRAY_OUT = -2;
+    final public static int ARRAY_NULL_ERROR_CODE = -3;
     final private static int ARRAY_MAXSIZE = 150000;
-    final private static int ELEMENT_OF_ARRAY_OUT = - 2;
     final private static int LEFT_BOUNDARY = -2147483648;
     final private static int RIGHT_BOUNDARY = 2147483647;
 
 
 
-    public static int solution(int[] A){
-        int maxSize, sizeA, index;
-        ArrayList<Integer> array = new ArrayList<Integer>();
-
-        if (A.length > ARRAY_MAXSIZE) {
+    public static int solution(long[] a){
+        if (a.length > ARRAY_MAXSIZE) {
             return ARRAY_OVERSIZE_ERROR_CODE;
-        } else if (A.length == 0) {
+        } else if (a.length == 0) {
             return ARRAY_NULL_ERROR_CODE;
         } else {
-            maxSize = 1;
-            index = 0;
-            sizeA = A.length;
+            ArrayList<Integer> array = new ArrayList<Integer>();
+            int maxSize = 1; // По условию задачи любой элемент является слайсом размером 1
+            int index = 0;   // и является возрастающим
+            int sizeA = a.length;
 
             for (int i=0; i < sizeA-1; i++) {
-                if (A[i] < LEFT_BOUNDARY || A[i] > RIGHT_BOUNDARY) {
+                if (a[i] < LEFT_BOUNDARY || a[i] > RIGHT_BOUNDARY) {
                     return ELEMENT_OF_ARRAY_OUT;
                 } else {
-                    if (A[i] < A[i+1]) {
+                    if (a[i] < a[i+1]) {
                         array.add(i);
                         if (maxSize < (array.size()+1)){
                             maxSize = array.size()+1;
@@ -42,13 +43,13 @@ public class Solution {
                     }
                 }
             }
-        return index;
+            return index;
         }
     }
 
 
     public static void main(String[] args) {
-        int[] array = new int[0];
+        long[] array = {1, 1, 1, 5, 4, 5, 6, 7, 5};
         int result = solution(array);
         switch (result){
             case ARRAY_OVERSIZE_ERROR_CODE:
--- src/test/home/ndrew2/SolutionTest.java
+++ src/test/home/ndrew2/SolutionTest.java
@@ -1,31 +1,32 @@
+package home.ndrew2;
+
+
 import org.junit.Test;
 
-import java.util.ArrayList;
-
+import static home.ndrew2.Solution.ARRAY_NULL_ERROR_CODE;
+import static home.ndrew2.Solution.ARRAY_OVERSIZE_ERROR_CODE;
+import static home.ndrew2.Solution.ELEMENT_OF_ARRAY_OUT;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
 
 
 /**
  * Created by Андрей on 03.06.2017.
  */
 public class SolutionTest{
-    final public static int ARRAY_OVERSIZE_ERROR_CODE = -1;
-    final public static int ARRAY_NULL_ERROR_CODE = -3;
-
 
     Solution solution = new Solution();
 
     //несколько тестов на проверку правильности поиска решений
     @Test
     public void testOnPositiveExpectedValues(){
-        int[] array1 = {0, 1, 2, 2, 1, 2, 1, 2, 5, 6, 6, 6, 6, 6};
+        long[] array1 = {0, 1, 2, 2, 1, 2, 1, 2, 5, 6, 6, 6, 6, 6};
         assertEquals(6, solution.solution(array1));
-        int[] array2 = {0, 1, 2, 3, 4, 4, 5 , 5, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+        long[] array2 = {0, 1, 2, 3, 4, 4, 5 , 5, 5, 6, 7, 8, 9, 10, 11, 12, 13};
         assertEquals(8, solution.solution(array2));
-        int[] array3 = {1};
+        long[] array3 = {1};
         assertEquals(0, solution.solution(array3));
-        int[] array4 = {1, 2, 4, 1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 16};
+        long[] array4 = {1, 2, 4, 1, 2, 4, 5, 7, 8, 9, 11, 13, 14, 15, 16};
         assertEquals(3, solution.solution(array4));
         array4[2] = -2;
         assertEquals(2, solution.solution(array4));
@@ -34,17 +35,24 @@ public class SolutionTest{
     //тест на нулевое количество элементов массива
     @Test
     public void testArrayNullSize() {
-        int[] array = new int[0];
+        long[] array = new long[0];
         assertEquals(ARRAY_NULL_ERROR_CODE, solution.solution(array));
     }
 
 
     //Тест на превышение размера массива выше допустимого значения.
     @Test
-    public void testConditionsOnOversize(){
-        int[] array = new int[150001];
+    public void testArraayOutOfRange(){
+        long[] array = new long[150001];
         assertEquals(ARRAY_OVERSIZE_ERROR_CODE, solution.solution(array));
     }
 
+    @Test
+    public void testElementInRange() {
+        long[] array = {0, 2147483648l, 0};
+        long[] array1 = {0, -2147483649l, 0};
+        assertEquals(ELEMENT_OF_ARRAY_OUT, solution.solution(array));
+        assertEquals(ELEMENT_OF_ARRAY_OUT, solution.solution(array1));
 
+    }
 }
